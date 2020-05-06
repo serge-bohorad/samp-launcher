b1b048197f3d0aad2794ab22a95a25763dff571a
Randell <irandellhart@gmail.com>
2020-05-06 14:29:12 +0300

    [Main] Update | Add new group events

---
+++ packages/main/app/core/events/group/index.ts
@@ -0,0 +1,28 @@
+import { EventManager } from '@app/libs/event-manager'
+import { Models } from '@app/core/models'
+
+EventManager.add('GROUP_FETCH', async () => {
+  const groups = await Models.Group.getAll()
+
+  return [groups, null]
+})
+
+EventManager.add('GROUP_CREATE', async (groupName) => {
+  await Models.Group.unsetSelected()
+
+  const group = await Models.Group.add(groupName)
+
+  return [group, null]
+})
+
+EventManager.add('GROUP_DELETE', async (groupId) => {
+  await Models.Group.deleteById(groupId)
+})
+
+EventManager.add('GROUP_RENAME', async ({ groupId, newGroupName }) => {
+  await Models.Group.rename(groupId, newGroupName)
+})
+
+EventManager.add('GROUP_SWITCH_SELECTED', async (groupId) => {
+  await Models.Group.switchSelected(groupId)
+})
