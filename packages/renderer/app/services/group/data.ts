4e2804dc59cc06288110bad9dd5a34f6ee59c88e
Randell <irandellhart@gmail.com>
2020-05-06 18:23:08 +0300

    [Renderer] Update | Fix group name reg exp

--- packages/renderer/app/services/group/data.ts
+++ packages/renderer/app/services/group/data.ts
@@ -2,7 +2,7 @@ const groupNameMinLength = 3
 const groupNameMaxLength = 30
 
 export const regexpGroupName = new RegExp(
-  `^[^\\s].{${groupNameMinLength - 2},${groupNameMaxLength}}[^\\s]$`
+  `^[^\\s].{${groupNameMinLength - 2},${groupNameMaxLength - 2}}[^\\s]$`
 )
 
 export const errorGroupNameInvalid = `Group name can consist of any chars without spaces at the beginning and end. Length: ${groupNameMinLength}-${groupNameMaxLength} chars.`
