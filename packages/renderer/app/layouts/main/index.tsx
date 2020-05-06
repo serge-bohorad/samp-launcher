4dc652ef2db07586eb9ef38cc081c63a3e042db6
Randell <irandellhart@gmail.com>
2020-05-06 14:43:10 +0300

    [Renderer] Update | Fetch groups from the main process at startup

--- packages/renderer/app/layouts/main/index.tsx
+++ packages/renderer/app/layouts/main/index.tsx
@@ -1,7 +1,9 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useCallback, useEffect } from 'react'
 import cn from 'classnames'
 
 import { Props } from './types'
+
+import { onFetchGroups } from '@app/services/group'
 
 import { PageHome } from '@app/pages/home'
 import { Titlebar } from './titlebar'
@@ -10,6 +12,14 @@ import styles from './styles.scss'
 
 export const LayoutMain: FunctionComponent<Props> = (props) => {
   const { className } = props
+
+  const init = useCallback(async () => {
+    await onFetchGroups()
+  }, [])
+
+  useEffect(() => {
+    init()
+  }, [])
 
   return (
     <div className={cn(styles.container, className)}>
