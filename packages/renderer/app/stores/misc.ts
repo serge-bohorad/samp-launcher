10ef16a6bde1ad9b33126607ec0ef6353ceb2e59
Randell <irandellhart@gmail.com>
2020-05-12 10:54:03 +0300

    [Renderer] Update | misc store

--- packages/renderer/app/stores/misc.ts
+++ packages/renderer/app/stores/misc.ts
@@ -1,10 +1,31 @@
 import { observable, action } from 'mobx'
 
+import { Notification } from '@app/types/notification'
+
 export class MiscStore {
   @observable usingCommonNickname = true
+  readonly notifications = observable<Pair<Notification>>([])
 
   @action setUsingCommonNickname = (state: boolean): void => {
     this.usingCommonNickname = state
+  }
+
+  @action addNotification = (notification: Notification, displayTime = 7000): void => {
+    this.notifications.push(Pair.create(notification))
+
+    if (displayTime > 0) {
+      const targetNotification = this.notifications.last()!
+
+      setTimeout(() => this.deleteNotification(targetNotification), displayTime)
+    }
+  }
+
+  @action deleteNotification = (notification: Pair<Notification>): void => {
+    this.notifications.remove(notification)
+  }
+
+  @action clearNotifications = (): void => {
+    this.notifications.clear()
   }
 }
 
