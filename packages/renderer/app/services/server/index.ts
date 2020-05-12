68890323386ef4bb59105eacbc1ac13b70e5600e
Randell <irandellhart@gmail.com>
2020-05-12 10:58:24 +0300

    [Renderer] Update | server service

--- packages/renderer/app/services/server/index.ts
+++ packages/renderer/app/services/server/index.ts
@@ -1,6 +1,8 @@
 import { Server } from '@shared/types/entities'
 
 import { errorAddressEmpty, defaultPort } from './data'
+import { settingsStore } from '@app/stores/settings'
+import { miscStore } from '@app/stores/misc'
 
 import { getSelectedGroup } from '@app/stores/group'
 import {
@@ -8,7 +10,14 @@ import {
   getSelectedServer,
   deleteServer,
   getServerAddress,
-  setServerAddress
+  setServerAddress,
+  setServerName,
+  setServerDescription,
+  setServerExtraInject,
+  setServerPing,
+  setServerInfo,
+  getFilteredServers,
+  setServerPassword
 } from '@app/stores/server'
 import { copyToClipboard } from '../misc'
 
@@ -22,11 +31,17 @@ export async function onAddServer(
 
   const [host, port] = splitServerAddress(serverAddress)
 
-  const [newServer] = await invokeMain('SERVER_ADD', { host, port, groupId: targetGroup!.id })
+  const [newServer, error] = await invokeMain('SERVER_ADD', {
+    host,
+    port,
+    groupId: targetGroup!.id
+  })
+
+  if (error) {
+    return error
+  }
 
   addServer(newServer!)
-
-  // TODO: refresh server
 }
 
 export function onDeleteServer(targetServer = getSelectedServer()): void {
@@ -56,11 +71,9 @@ export async function onCloneServer(targetServer = getSelectedServer()): Promise
 
   const [newServer] = await invokeMain('SERVER_CLONE', targetServer.id)
 
+  setServerExtraInject(newServer!, Pair.from(newServer!.extraInject as string[]))
+
   addServer(newServer!)
-
-  // TODO: Process extra inject
-
-  // TODO: refresh server
 }
 
 export function onSaveServerNickname(
@@ -77,27 +90,156 @@ export function onSaveServerNickname(
   })
 }
 
-export function onSaveServerAddress(
-  newServerAddress: string,
+export async function onSaveServerAddress(
+  newAddress: string,
   targetServer = getSelectedServer()
-): string | void {
-  if (!targetServer || getServerAddress(targetServer).equal(newServerAddress)) {
+): Promise<string | void> {
+  if (!targetServer || getServerAddress(targetServer).equal(newAddress)) {
     return
   }
 
-  if (!newServerAddress) {
+  if (!newAddress) {
     return errorAddressEmpty
   }
 
-  const [host, port] = splitServerAddress(newServerAddress)
+  const [host, port] = splitServerAddress(newAddress)
 
-  invokeMainUnilaterally('SERVER_UPDATE_ADDRESS', { serverId: targetServer.id, host, port })
+  const [, error] = await invokeMain('SERVER_UPDATE_ADDRESS', {
+    serverId: targetServer.id,
+    host,
+    port
+  })
+
+  if (error) {
+    return error
+  }
 
   setServerAddress(targetServer, host, port)
 }
 
-function splitServerAddress(serverAddress: string): [string, number] {
-  const [host, rawPort] = serverAddress.split(':')
+export function onSaveServerName(
+  newName: string,
+  targetServer = getSelectedServer()
+): string | void {
+  if (!targetServer || targetServer.name.equal(newName)) {
+    return
+  }
+
+  invokeMainUnilaterally('SERVER_UPDATE_NAME', { serverId: targetServer.id, name: newName })
+
+  setServerName(targetServer, newName)
+}
+
+export function onSaveServerDescription(
+  newDescription: string,
+  targetServer = getSelectedServer()
+): string | void {
+  if (!targetServer || targetServer.description.equal(newDescription)) {
+    return
+  }
+
+  invokeMainUnilaterally('SERVER_UPDATE_DESCRIPTION', {
+    serverId: targetServer.id,
+    description: newDescription
+  })
+
+  setServerDescription(targetServer, newDescription)
+}
+
+export function onSaveServerExtraInject(
+  newExtraInject: Pair<string>[],
+  targetServer = getSelectedServer()
+): void {
+  if (!targetServer) {
+    return
+  }
+
+  invokeMainUnilaterally('SERVER_UPDATE_EXTRA_INJECT', {
+    serverId: targetServer.id,
+    extraInject: Pair.values(newExtraInject)
+  })
+
+  setServerExtraInject(targetServer, newExtraInject)
+}
+
+export async function onRefreshServer(targetServer = getSelectedServer()): Promise<void> {
+  if (!targetServer) {
+    return
+  }
+
+  const { host, port } = targetServer
+
+  const [ping] = await invokeMain('SERVER_GET_PING', { host, port })
+
+  if (ping) {
+    setServerPing(targetServer, ping)
+  }
+
+  const [info] = await invokeMain('SERVER_GET_INFO', {
+    host,
+    port
+  })
+
+  if (info) {
+    setServerInfo(targetServer, info)
+  }
+}
+
+export function onRefreshSeveralServers(targetServers = getFilteredServers()): Promise<void[]> {
+  return Promise.all(targetServers.map((server) => onRefreshServer(server)))
+}
+
+export function onSaveServerPassword(
+  newPassword: string,
+  targetServer = getSelectedServer()
+): void {
+  if (!targetServer || targetServer.password.equal(newPassword)) {
+    return
+  }
+
+  invokeMainUnilaterally('SERVER_UPDATE_PASSWORD', {
+    serverId: targetServer.id,
+    password: newPassword
+  })
+
+  setServerPassword(targetServer, newPassword)
+}
+
+export async function onConnectToServer(
+  targetServer = getSelectedServer()
+): Promise<string | void> {
+  if (!targetServer) {
+    return
+  }
+
+  const { usingCommonNickname } = miscStore
+  const { commonNickname, commonExtraInject, gameDirectory } = settingsStore
+  const {
+    host,
+    port,
+    nickname: serverNickname,
+    password,
+    extraInject: serverExtraInject
+  } = targetServer
+  const nickname = usingCommonNickname ? commonNickname : serverNickname
+
+  const extraInject = serverExtraInject.isEmpty() ? commonExtraInject : serverExtraInject
+  const finalExtraInject = Pair.values(extraInject as Pair<string>[])
+
+  const [, error] = await invokeMain('GAME_RUN', {
+    host,
+    port,
+    nickname,
+    password,
+    extraInject: finalExtraInject,
+    gameDirectory
+  })
+
+  return error!
+}
+
+function splitServerAddress(address: string): [string, number] {
+  const [host, rawPort] = address.trim().split(':')
   const port = Number(rawPort) || defaultPort
 
   return [host, port]
@@ -114,22 +256,20 @@ function generateServerInfo(targetServer: Server): string {
     ping,
     mode,
     language,
-    map,
+    mapname,
     weburl,
     version
   } = targetServer
 
-  const info = [
+  return [
     `Hostname: ${name || hostname || '-'}`,
     `Address: ${host}:${port}`,
     `Players: ${players || 0} / ${maxPlayers || 0}`,
     `Ping: ${ping || 0}`,
     `Mode: ${mode || '-'}`,
     `Language: ${language || '-'}`,
-    `Map: ${map || '-'}`,
+    `Map: ${mapname || '-'}`,
     `Web-site: ${weburl || '-'}`,
     `Version: ${version || '-'}`
-  ]
-
-  return info.join('\n')
+  ].join('\n')
 }
